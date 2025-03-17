ab26188003e86fa9ad2001fccd6c6e54a539774f
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-03-17 09:12:49 +0000

    update

--- push_swap.c
+++ push_swap.c
@@ -25,8 +25,27 @@ int main(int ac, char *av[])
 		if ((stack_a->index > stack_a->next->index))
 			swap(&stack_a, 'a');
 	}
+	if (size == 3)
+	{
+		if (stack_a->index == (size - 1))
+			rotate(&stack_a, 'a');
+		else if (stack_a->index < stack_a->next->index)
+			reverse_rotate(&stack_a, 'a');
+		if ((stack_a->index > stack_a->next->index))
+			swap(&stack_a, 'a');
+	}
 	print_node(stack_a, 'A');
 }
 
 // If the first element isn’t the largest,
 // check if the second element is. Compare their index. 
+
+void sort_three(t_stack **stack, int size)
+{
+	if ((*stack)->index == (size - 1))
+		rotate(stack);
+	else if ((*stack)->index < (*stack)->next->index)
+		reverse_rotate(stack);
+	if ((*stack)->index > (*stack)->next->index)
+		swap(stack);
+}

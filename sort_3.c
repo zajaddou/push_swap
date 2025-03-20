69b27cafc6197427008b55ab3546d915cd34a3a9
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-03-20 07:03:20 +0000

    update

--- sort_3.c
+++ sort_3.c
@@ -3,14 +3,21 @@
 
 void sort_3(t_stack **stack, char c)
 {
-    int n1 = (*stack)->index;
-    int n2 = (*stack)->next->index;
-    int n3 = (*stack)->next->next->index;
+    int n1;
+    int n2;
+    int n3;
 
-    if (n1 > n2 && n1 > n3)
-        rotate(stack, c);
-    else if (n2 > n1 && n2 > n3)
-        reverse_rotate(stack, c);
+    if (stack_size(*stack) == 3)
+    {
+        n1 = (*stack)->index;
+        n2 = (*stack)->next->index;
+        n3 = (*stack)->next->next->index;
+
+        if (n1 > n2 && n1 > n3)
+            rotate(stack, c);
+        else if (n2 > n1 && n2 > n3)
+            reverse_rotate(stack, c);
+    }
     if ((*stack)->index > (*stack)->next->index)
         swap(stack, c);
 }

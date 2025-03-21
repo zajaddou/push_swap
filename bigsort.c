3b919359c349f76d32407d8e8e6a4784739f63ca
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-03-21 09:15:07 +0000

    update

--- bigsort.c
+++ bigsort.c
@@ -1,13 +1,11 @@
 
 #include "push_swap.h"
 
-void	big_sort(t_stack **stack_a, t_stack **stack_b)
+void	big_sort(t_stack **stack_a, t_stack **stack_b, int range)
 {
 	int	i;
-	int range;
 
 	i = 0;
-	range = 18;
 	while (*stack_a)
 	{
 		if ((*stack_a)->index <= i)
@@ -15,10 +13,11 @@ void	big_sort(t_stack **stack_a, t_stack **stack_b)
 			push(stack_a, stack_b);
 			i++;
 		}
-		else if ((*stack_a)->index <= i + range)
+		else if ((*stack_a)->index <= (i + range))
 		{
 			push(stack_a, stack_b);
 			rotate(stack_b);
+			i++;
 		}
 		else
 			rotate(stack_a);

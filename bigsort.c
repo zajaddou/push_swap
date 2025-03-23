9e49da21ddd9af38e4a62bb1bfc358a3f0e58b47
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-03-23 08:56:20 +0000

    update

--- bigsort.c
+++ bigsort.c
@@ -1,25 +1,66 @@
 
 #include "push_swap.h"
 
-void	big_sort(t_stack **stack_a, t_stack **stack_b, int range)
+static void swap_int(int *a, int *b)
 {
-	int	i;
+	int tmp;
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
-	i = 0;
-	while (*stack_a)
+static void selection_sort(int *list, int size)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < size)
 	{
-		if ((*stack_a)->index <= i)
+		b = (a + 1);
+		while (b < size)
 		{
-			push(stack_a, stack_b);
-			i++;
+			if (list[a] > list[b])
+				swap_int(&list[a], &list[b]);
+			b++;
 		}
-		else if ((*stack_a)->index <= (i + range))
-		{
-			push(stack_a, stack_b);
-			rotate(stack_b);
-			i++;
-		}
-		else
-			rotate(stack_a);
+		a++;
 	}
+}
+
+static int	*stack_to_list(t_stack *stack, int size)
+{
+	t_stack	*node;
+	int		*list;
+	int		i;
+
+	node = stack;
+	list = ft_malloc(sizeof(int) * size);
+	i = 0;
+	while (node->next != NULL)
+	{
+		list[i++] = node->data;
+		node = node->next;
+	}
+	list[i] = node->data;
+
+	selection_sort(list, size);
+
+	return (list);
+}
+
+void	big_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size;
+	int	*list;
+	int	i;
+	(void)stack_b;
+
+	size = stack_size(*stack_a);
+	list = stack_to_list(*stack_a, size);
+
+	i = -1;
+	while (++i < size)
+		printf("%d ", list[i]);
 }
--- push_swap.c
+++ push_swap.c
@@ -16,10 +16,8 @@ int	main(int ac, char *av[])
 		sort_2_3(&stack_a);
 	else if (size == 4 || size == 5)
 		sort_4_5(&stack_a, &stack_b);
-	else if (size <= 100)
-		big_sort(&stack_a, &stack_b, 15);
-	else if (size > 100)
-		big_sort(&stack_a, &stack_b, 42);
+	else
+		big_sort(&stack_a, &stack_b);
 
 	//print_node(stack_a);
 	//print_node(stack_b);
